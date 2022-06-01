220d9172595c8a457e47c2a505a43b67408af363
Ron Lara <lararon2428@gmail.com>
2022-06-01 13:50:21 +0800

    updated navbar function styles + footer

--- app.js
+++ app.js
@@ -9,9 +9,9 @@ const methodOverride = require('method-override');
 const app = express();
 //Static Files
 app.use(express.static('public'));
-app.use('/css',express.static(__dirname + 'public/css'))
-app.use('/js',express.static(__dirname + 'public/js'))
-app.use('/img',express.static(__dirname + 'public/img'))
+app.use('/css', express.static(__dirname + 'public/css'))
+app.use('/js', express.static(__dirname + 'public/js'))
+app.use('/img', express.static(__dirname + 'public/img'))
 
 app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
@@ -20,13 +20,24 @@ app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'))
 
 app.get('/', (req, res) => {
-    res.render('bagotayo/index');
+    nav_hl = "home";
+    res.render('bagotayo/index', { nav_hl });
 })
 app.get('/contact', (req, res) => {
-    res.render('bagotayo/contact');
+    nav_hl = "contact";
+    res.render('bagotayo/contact',{nav_hl});
 })
 app.get('/forum', (req, res) => {
-    res.render('bagotayo/forum');
+    nav_hl = "forum";
+    res.render('bagotayo/forum',{nav_hl});
+})
+app.get('/about', (req, res) => {
+    nav_hl = "about";
+    res.render('bagotayo/about',{nav_hl});
+})
+app.get('/resources', (req, res) => {
+    nav_hl = "resources";
+    res.render('bagotayo/resources',{nav_hl});
 })
 app.listen(3000, () => {
     console.log("Serving on port 3000");
