69977c24d450500c6ae1e2258cbf1919389ab59e
Ron Lara <lararon2428@gmail.com>
2022-06-08 16:42:59 +0800

    update index of featured videos or publications, added search feature

--- app.js
+++ app.js
@@ -7,11 +7,11 @@ const methodOverride = require('method-override');
 const catchAsync = require('./utils/catchAsync');
 const ExpressError = require('./utils/ExpressError');
 
-const {google} = require("googleapis");
+const { google } = require("googleapis");
 const spreadsheetId = "1iJGfuIR6TjAD74CbqdgBcd2RPqMw02b0zitzj_JbOXY";
 const auth = new google.auth.GoogleAuth({
-    keyFile:"credentials.json",
-    scopes:"https://www.googleapis.com/auth/spreadsheets",
+    keyFile: "credentials.json",
+    scopes: "https://www.googleapis.com/auth/spreadsheets",
 });
 
 
@@ -26,90 +26,144 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'))
 
-async function getData(sheet){
+async function getData(sheet) {
     const client = await auth.getClient();
-    const googleSheets = google.sheets({version:"v4",auth:client});
+    const googleSheets = google.sheets({ version: "v4", auth: client });
     const getRows = await googleSheets.spreadsheets.values.get({
         auth,
         spreadsheetId,
-        range:sheet,
+        range: sheet,
     });
     var Rowdata = getRows.data.values;
-    Rowdata.splice(0,1);
+    Rowdata.splice(0, 1);
     return Rowdata;
 }
 
-const validateContact = (req,res,next) =>{
-    const {error} = contactSchema.validate(req.body);
-    if(error){
+const validateContact = (req, res, next) => {
+    const { error } = contactSchema.validate(req.body);
+    if (error) {
         const msg = error.details.map(el => el.message).join(',');
-        throw new ExpressError(msg,400);
-    }else{
+        throw new ExpressError(msg, 400);
+    } else {
         next();
     }
 }
 
-app.get('/', catchAsync(async(req, res ,next) => {
+app.get('/', catchAsync(async (req, res, next) => {
+    const client = await auth.getClient();
+    const googleSheets = google.sheets({ version: "v4", auth: client });
+    var get_temps = await googleSheets.spreadsheets.values.batchGet({
+        auth,
+        spreadsheetId,
+        ranges: ["Videos","Publications"],
+    });
+    var vids = get_temps.data.valueRanges[0].values;
+    var pubs = get_temps.data.valueRanges[1].values;
+    vids.splice(0, 1); pubs.splice(0, 1);
+    for(let v of vids){
+        v.push("Videos")
+    }
+    for(let p of pubs){
+        p.push("Publications")
+    }
+    var arr = vids.concat(pubs)
+    var swap;
+    for(let x of arr){
+        swap = x[0];
+        x[0] = x[2]
+        x[2] = swap;
+    }
+    arr.sort().reverse();
+    var numarr = 6;
+    if(arr.length < 6){
+        numarr = arr.length
+    }
     nav_hl = "home";
-    var video_data = await getData("Videos");
-    res.render('bagotayo/index', { nav_hl,video_data});
+    res.render('bagotayo/index', { nav_hl, arr, numarr});
 }))
-app.get('/contact', (req, res ,next) => {
+app.get('/contact', (req, res, next) => {
     nav_hl = "contact";
     res.render('bagotayo/contact', { nav_hl });
 })
-app.post('/contact', validateContact, catchAsync(async(req,res ,next)=>{
+app.post('/contact', validateContact, catchAsync(async (req, res, next) => {
     nav_hl = "";
-    const {name,email,pnum,msg} = req.body
+    const { name, email, pnum, msg } = req.body
     const client = await auth.getClient();
-    const googleSheets = google.sheets({version:"v4",auth:client});
+    var today = new Date();
+    var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+    var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    var dateTime = date + ' ' + time;
+    const googleSheets = google.sheets({ version: "v4", auth: client });
     await googleSheets.spreadsheets.values.append({
         auth,
         spreadsheetId,
-        range:"Contact",
-        valueInputOption:"USER_ENTERED",
-        resource:{
-            values:[[name,email,pnum,msg]],
+        range: "Contact",
+        valueInputOption: "USER_ENTERED",
+        resource: {
+            values: [[dateTime, name, email, pnum, msg]],
         },
     });
-    res.render('bagotayo/contactsubmitted', {nav_hl});
+    res.render('bagotayo/contactsubmitted', { nav_hl });
 }))
-app.get('/forum', (req, res ,next) => {
+app.get('/search', catchAsync(async (req, res, next) => {
+    const client = await auth.getClient();
+    const googleSheets = google.sheets({ version: "v4", auth: client });
+    var get_temps = await googleSheets.spreadsheets.values.batchGet({
+        auth,
+        spreadsheetId,
+        ranges: ["Templates", "Videos", "Publications", "Other Materials"],
+    });
+    var temps = get_temps.data.valueRanges[0].values;
+    var vids = get_temps.data.valueRanges[1].values;
+    var pubs = get_temps.data.valueRanges[2].values;
+    var others = get_temps.data.valueRanges[3].values;
+    temps.splice(0, 1); vids.splice(0, 1); pubs.splice(0, 1); others.splice(0, 1);
+    var data_q = {
+        "Templates": temps,
+        "Videos": vids,
+        "Publications": pubs,
+        "Other Materials": others,
+    }
+    nav_hl = ""
+    var query = req.query.q;
+    res.render('bagotayo/search', { nav_hl, query, data_q })
+}))
+app.get('/forum', (req, res, next) => {
     nav_hl = "forum";
     res.render('bagotayo/forum', { nav_hl });
 })
-app.get('/about', (req, res ,next) => {
+app.get('/about', (req, res, next) => {
     nav_hl = "about";
     res.render('bagotayo/about', { nav_hl });
 })
-app.get('/resources/templates', catchAsync(async(req, res ,next) => {
+app.get('/resources/templates', catchAsync(async (req, res, next) => {
     nav_hl = "resources";
     var temps_data = await getData("Templates");
     var dict_temps = {}
-    for(let i=0;i<temps_data.length;i++){
-        if(temps_data[i][0] in dict_temps){
-            dict_temps[temps_data[i][0]].push([temps_data[i][1],temps_data[i][2]]);
+    for (let i = 0; i < temps_data.length; i++) {
+        if (temps_data[i][0] in dict_temps) {
+            dict_temps[temps_data[i][0]].push([temps_data[i][1], temps_data[i][3]]);
         }
-        else{
-            dict_temps[temps_data[i][0]] = [[temps_data[i][1],temps_data[i][2]]];
+        else {
+            dict_temps[temps_data[i][0]] = [[temps_data[i][1], temps_data[i][3]]];
         }
     }
     res.render('bagotayo/res_templates', { nav_hl, dict_temps });
 }))
-app.get('/resources/videos', catchAsync(async(req, res ,next) => {
+app.get('/resources/videos', catchAsync(async (req, res, next) => {
     nav_hl = "resources";
     var video_data = await getData("Videos");
-    res.render('bagotayo/res_videos', { nav_hl,video_data });
+    res.render('bagotayo/res_videos', { nav_hl, video_data });
 }))
-app.get('/resources/publications', catchAsync(async(req, res ,next) => {
+app.get('/resources/publications', catchAsync(async (req, res, next) => {
     nav_hl = "resources";
     var pub_data = await getData("Publications");
-    res.render('bagotayo/res_pub', { nav_hl,pub_data });
+    res.render('bagotayo/res_pub', { nav_hl, pub_data });
 }))
-app.get('/resources/othermaterials', catchAsync(async(req, res ,next) => {
+app.get('/resources/othermaterials', catchAsync(async (req, res, next) => {
     nav_hl = "resources";
     var oth_data = await getData("Other Materials");
-    res.render('bagotayo/res_others', { nav_hl, oth_data});
+    res.render('bagotayo/res_others', { nav_hl, oth_data });
 }))
 app.all('*', (req, res, next) => {
     next(new ExpressError('Page Not Found', 404));
